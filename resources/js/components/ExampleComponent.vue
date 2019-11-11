580480801c0e4eda03a3a1781e985e43479328f4
aldiandika <aldi.andika@gmail.com>
2019-11-11 10:44:26 +0700

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Example Component</div>
+
+          <div class="card-body">testing lagi</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+  created() {
+    Echo.channel("location").listen("SendLocation", e => {
+      //
+    });
+  }
+};
+</script>
