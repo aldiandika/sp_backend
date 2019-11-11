6e879d21ddaaf7f6358d1d0db77d66b2b75b314c
aldiandika <aldi.andika@gmail.com>
2019-11-11 13:31:25 +0700

    membuat database marker, command, dan tracker

---
+++ app/CommandData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommandData extends Model
+{
+    private $CommandTable = 'command_datas';
+}
---
+++ database/migrations/2019_11_11_062012_create_command_datas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommandDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('command_datas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('array_lat');
+            $table->string('array_lon');
+            $table->string('kategori');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('command_datas');
+    }
+}
